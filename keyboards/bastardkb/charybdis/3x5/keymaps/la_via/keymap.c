464b49cd1cdde55c930d63c52bbc668290788108
Laurent Wirz <lwirz@lwirz.ch>
2025-05-24 17:31:24 +0200

    minor spacing adaption

--- keyboards/bastardkb/charybdis/3x5/keymaps/la_via/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/la_via/keymap.c
@@ -130,8 +130,8 @@ static uint16_t auto_pointer_layer_timer = 0;
 /** \brief QWERTY layout (3 rows, 10 columns). */
 #define LAYOUT_LAYER_BASE                                                                     \
        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_COMBO_1,    KC_COMBO_2,    KC_O,    KC_COMBO_3, \
-       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, \
-       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, \
+       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,          KC_K,          KC_L,    KC_SCLN, \
+       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,          KC_COMM,       KC_DOT,  KC_SLSH, \
                       ESC_MED, SPC_NAV, TAB_FUN, ENT_SYM, BSP_NUM
 
 /** Convenience row shorthands. */
